7e873659c600d6249f20b49d9ba9940094186c57
leokatsikaris <leonelkatsikaris@gmail.com>
2022-01-19 10:37:09 -0300

    prototipo de info mostrada hecho

--- crypto-dashboard/src/components/Dashboard/Dashboard.jsx
+++ crypto-dashboard/src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 
 export function Dashboard() {
+    const [info, setInfo] = useState([]);
+
+
+
+    useEffect(async () => {
+        let interactionAPI = await axios.get('https://api2.binance.com/api/v3/ticker/24hr');
+        await setInfo(interactionAPI.data);
+      }, []);
+
+
+
     return (
-            <div>Acá va a ir la info</div>
+            <div>{info.map(c => {
+                return (
+                <div>
+                <p>{c.symbol}</p>
+                <p>{c.priceChange}</p>
+                </div>
+                )
+            })}</div>
     )
 }
