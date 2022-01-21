a6f125afc7ee8ddafa13ef07ecfb22cefc0070f3
leokatsikaris <leonelkatsikaris@gmail.com>
2022-01-21 02:56:37 -0300

    hecho modelo de tabla con material ui

--- crypto-dashboard/src/components/Dashboard/Dashboard.jsx
+++ crypto-dashboard/src/components/Dashboard/Dashboard.jsx
@@ -1,28 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Cryptocard } from '../Cryptocard/Cryptocard';
-
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export function Dashboard() {
-    const [info, setInfo] = useState([]);
+  const [info, setInfo] = useState([]);
 
+  useEffect(() => {
+      async function fetchData () {
+          let interactionAPI = await axios.get('https://api2.binance.com/api/v3/ticker/24hr');
+          await setInfo(interactionAPI.data);
+      }
+      fetchData();
+    }, []);
 
-
-    useEffect(() => {
-        async function fetchData () {
-            let interactionAPI = await axios.get('https://api2.binance.com/api/v3/ticker/24hr');
-            await setInfo(interactionAPI.data);
-        }
-        fetchData();
-      }, []);
-
-
-
-    return (
-            <div>{info.map(c => {
-                return (
-                <Cryptocard symbol={c.symbol} priceChange={c.priceChange} />
-                )
-            })}</div>
-    )
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Crypto</TableCell>
+            <TableCell align="right">Price Change</TableCell>
+            <TableCell align="right">Volume</TableCell>
+            <TableCell align="right">Last Price</TableCell>
+            <TableCell align="right">Bid Price</TableCell>
+            <TableCell align="right">High Price</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {info.map((c) => (
+            <TableRow
+              key={c.firstId}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {c.symbol}
+              </TableCell>
+              <TableCell align="right">{c.priceChange}</TableCell>
+              <TableCell align="right">{c.volume}</TableCell>
+              <TableCell align="right">{c.lastPrice}</TableCell>
+              <TableCell align="right">{c.bidPrice}</TableCell>
+              <TableCell align="right">{c.highPrice}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
