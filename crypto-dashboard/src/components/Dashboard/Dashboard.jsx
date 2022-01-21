8780228e2ff6a1df4bd679890eeb057c4cffc968
leokatsikaris <leonelkatsikaris@gmail.com>
2022-01-21 12:18:27 -0300

    info elegida y paginacion de la tabla hecha

--- crypto-dashboard/src/components/Dashboard/Dashboard.jsx
+++ crypto-dashboard/src/components/Dashboard/Dashboard.jsx
@@ -7,49 +7,76 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import TablePagination from '@mui/material/TablePagination';
+import Pagination from '@mui/material/Pagination';
 
 export function Dashboard() {
   const [info, setInfo] = useState([]);
-
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  
   useEffect(() => {
-      async function fetchData () {
-          let interactionAPI = await axios.get('https://api2.binance.com/api/v3/ticker/24hr');
-          await setInfo(interactionAPI.data);
-      }
-      fetchData();
-    }, []);
+    async function fetchData () {
+      let interactionAPI = await axios.get('https://api2.binance.com/api/v3/ticker/24hr');
+      await setInfo(interactionAPI.data);
+    }
+    fetchData();
+  }, []);
+
+  
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  
+
 
   return (
+    <div>
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>Crypto</TableCell>
-            <TableCell align="right">Price Change</TableCell>
-            <TableCell align="right">Volume</TableCell>
+            <TableCell align="right">Price</TableCell>
             <TableCell align="right">Last Price</TableCell>
-            <TableCell align="right">Bid Price</TableCell>
-            <TableCell align="right">High Price</TableCell>
+            <TableCell align="right">Price change</TableCell>
+            <TableCell align="right">Price change %</TableCell>
+            <TableCell align="right">Volume</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {info.map((c) => (
+          {info.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+          .map((c) => (
             <TableRow
-              key={c.firstId}
+              key={c.symbol}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
                 {c.symbol}
               </TableCell>
-              <TableCell align="right">{c.priceChange}</TableCell>
-              <TableCell align="right">{c.volume}</TableCell>
-              <TableCell align="right">{c.lastPrice}</TableCell>
-              <TableCell align="right">{c.bidPrice}</TableCell>
-              <TableCell align="right">{c.highPrice}</TableCell>
+              <TableCell align="right">${c.openPrice}</TableCell>
+              <TableCell align="right">${c.lastPrice}</TableCell>
+              <TableCell align="right">${c.priceChange}</TableCell>
+              <TableCell align="right">{c.priceChangePercent} %</TableCell>
+              <TableCell align="right">${c.volume}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
+      <TablePagination
+      component="div"
+      count={info?.length + 1}
+      page={page}
+      onPageChange={handleChangePage}
+      rowsPerPage={rowsPerPage}
+      onRowsPerPageChange={handleChangeRowsPerPage}
+    />
     </TableContainer>
+    </div>
   );
 }
